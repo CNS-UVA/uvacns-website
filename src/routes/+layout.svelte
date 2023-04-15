9c3244310647d51fe9409038d9f6faf7609f400a
Neil Phan <nnp3axx@virginia.edu>
2023-04-14 22:20:34 -0400

    Added header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,61 @@
 	// If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
 	import '@skeletonlabs/skeleton/styles/all.css';
 	// Most of your app wide CSS should be put in this file
-	import '../app.postcss';
+    import '../app.postcss';
+    import { AppShell, AppBar } from '@skeletonlabs/skeleton';
 </script>
 
-<slot />
+<AppShell>
+	<svelte:fragment slot="header">
+		<!-- App Bar -->
+		<AppBar>
+            <svelte:fragment slot="lead">
+                <a
+                    href="/"
+                >
+                <strong class="text-xl uppercase">
+                    Computer Network and Security Club
+                </strong>
+                </a>
+			</svelte:fragment>
+			<svelte:fragment slot="trail">
+				<a
+					class="btn variant-ghost-surface"
+					href="/"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Home
+				</a>
+				<a
+					class="btn variant-ghost-surface"
+					href="/resources"
+					target="_blank"
+					rel="noreferrer"
+				>
+				    Resources
+				</a>
+				<a
+					class="btn variant-ghost-surface"
+					href="/officers"
+					target="_blank"
+					rel="noreferrer"
+				>
+				    Officers
+				</a>
+				<a
+					class="btn variant-ghost-surface"
+					href="https://discord.gg/87aKE8gqSc"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Discord
+				</a>
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<!-- Page Route Content -->
+	<slot />
+</AppShell>
+
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,15 @@
 <!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
 
 <div class="container h-full mx-auto flex justify-center items-center">
-	<div class="space-y-5">
-		<h1>Let's get cracking bones!</h1>
-		<p>Start by exploring:</p>
-		<ul>
-			<li><code>/src/routes/+layout.svelte</code> - barebones layout, the CSS import order is critical!</li>
-			<li><code>/src/app.postcss</code> - minimal css to make the page full screen, may not be relevant for your project</li>
-			<li><code>/src/routes/+page.svelte</code> - this page, you can replace the contents</li>
-		</ul>
+    <div class="space-y-0 flex justify-evenly gap-10">
+        <div class="space-y-5"> 
+		    <h1>About Us</h1>
+		    <p>We are a group of UVA students interested in software, network, hardware, and application security. We engage in security competitions as a club, host interesting speakers, develop security tools and challenges, and leverage industry connections.</p>
+        </div>
+
+        <div class="space-y-5">
+            <h1>What do we do?</h1>
+		    <p>We are a group of UVA students interested in software, network, hardware, and application security. We engage in security competitions as a club, host interesting speakers, develop security tools and challenges, and leverage industry connections.</p>
+        </div>
 	</div>
 </div>
