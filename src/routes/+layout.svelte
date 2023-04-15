2be8f8fa3e3ed14d04ad9e9ad2576264b6a91524
Neil Phan <nnp3axx@virginia.edu>
2023-04-14 22:28:36 -0400

    Added footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -57,7 +57,13 @@
 			</svelte:fragment>
 		</AppBar>
 	</svelte:fragment>
-	<!-- Page Route Content -->
+    <!-- Page Route Content -->
+    <svelte:fragment slot="footer">
+        <AppBar>
+                <p class="flex items-center">Although this organization has members who are University of Virginia students and may have, University employees associated or engaged in its activities and affairs, the organization is not a part of or an agency. It is a separate and independent organization, which is responsible for and manages its own activities and affairs. The University does not direct, supervise or control the organization and is not responsible for the organization’s contracts, acts or omissions. </p>
+                <p> © CNS@UVA </p>
+        </AppBar>
+    </svelte:fragment>
 	<slot />
 </AppShell>
 
