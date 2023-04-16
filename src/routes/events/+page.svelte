7844493f3dffa3f23130746f30f54fc06452ee0a
Neil Phan <nnp3axx@virginia.edu>
2023-04-16 00:13:54 -0400

    Updated events and resources page

---
+++ src/routes/events/+page.svelte
@@ -0,0 +1,39 @@
+
+<div class="p-24 container mx-auto flex-column [&>*]:px-8">
+
+    <h1 class="p-8"> Here are some of the things that go on around here. </h1>
+
+    <div class="p-4 [&>*]:py-1">
+        <h2> Weekly Meetings </h2>
+        <ul>
+            <li> <b>When:</b> Wednesdays at 6:00PM EST </li>
+            <li> <b>Location:</b> Olsson Hall 018 </li>
+            <li>  <b>Description:</b>
+                    Each week, we have something new to show to all of our members at CNS. Ranging from mini capture-the-flag competitions, to talks about binary exploitation, our weekly meetings are a great way to dive deep into the realm of cybersecurity. We also leverage our connections to introduce speakers that have served in the security industry to give you the information you need to succeed.
+            </li>
+        </ul>
+    </div>
+
+    <div class="p-4 [&>*]:py-1">
+        <h2> CyberStorm </h2>
+        <ul>
+            <li> <b>Website:</b> <a href="https://cyberstorm.uvacns.com/"> https://cyberstorm.uvacns.com/ </a> </li>
+            <li> <b>Contact:</b> <a href="mailto:cah3ks@virginia.edu"> cah3ks@virginia.edu </a> </li>
+            <li> <b>Description:</b> 
+                CyberStorm is a red versus blue team competition that simulates real-world cyber attacks in a controlled environment. Participants (blue teams) are responsible for maintaining business operations while warding off an active cyber threat, simulated by the red team. Hosted by CNS@UVA every semester, it is the biggest university event that tests your abilities to defend against cyber-attack scenarios and keep you on your feet.
+            </li>
+        </ul>
+    </div>
+
+
+    <div class="p-4 [&>*]:py-1">
+        <h2> Collegiate Cyber Defense Competition</h2>
+        <ul>
+            <li> <b>Nationals Website:</b> <a href="https://www.nationalccdc.org/"> https://www.nationalccdc.org/ </a> </li>
+            <li> <b>Regionals Website:</b> <a href="https://maccdc.org/"> https://maccdc.org/ </a> </li>
+            <li> <b>Description:</b> 
+                The Collegiate Cyber Defense Competition (CCDC) is an annual event that challenges college and university students to protect a simulated corporate network from cyberattacks. The competition involves creating and defending a network against a red team of professional hackers while maintaining critical services for a simulated business. The teams are scored based on their ability to identify and mitigate security risks, respond to incidents, and maintain service availability.
+            </li>
+        </ul>
+    </div>
+</div>
--- src/routes/resources/+page.svelte
+++ src/routes/resources/+page.svelte
@@ -5,16 +5,6 @@
 <div class="p-24 container mx-auto flex-column [&>*]:px-8">
     <h1 class="p-8"> Here are some resources we're particularly excited about. </h1>
     <div class="p-4 [&>*]:py-1">
-        <h2> CyberStorm </h2>
-        <ul>
-            <li> <b>Website:</b> <a href="https://cyberstorm.uvacns.com/"> https://cyberstorm.uvacns.com/ </a> </li>
-            <li> <b>Contact:</b> <a href="mailto:cah3ks@virginia.edu"> cah3ks@virginia.edu </a> </li>
-            <li> <b>Description:</b> 
-                CyberStorm is a red versus blue team competition that simulates real-world cyber attacks in a controlled environment. Participants (blue teams) are responsible for maintaining business operations while warding off an active cyber threat, simulated by the red team. Hosted by CNS@UVA every semester, it is the biggest university event that tests your abilities to defend against cyber-attack scenarios and keep you on your feet.
-            </li>
-        </ul>
-    </div>
-    <div class="p-4 [&>*]:py-1">
         <h2> MetaCTF </h2>
         <ul>
             <li> <b>Website:</b> <a href="https://metactf.com/"> https://metactf.com/ </a> </li>
@@ -28,16 +18,6 @@
         <ul>
             <li> <b>Website:</b> <a href="https://www.hackthebox.com/"> https://www.hackthebox.com/ </a> </li>
             <li> <b>Description:</b> Hack The Box is an organization designing a gamified software platform for cybersecurity upskilling and talent assessment. Their goal is to allow individuals, businesses, government institutions, and universities sharpen both their offensive and defensive cybersecurity skills while making it an engaging and fun experience. 
-            </li>
-        </ul>
-    </div>
-    <div class="p-4 [&>*]:py-1">
-        <h2> Collegiate Cyber Defense Competition</h2>
-        <ul>
-            <li> <b>Nationals Website:</b> <a href="https://www.nationalccdc.org/"> https://www.nationalccdc.org/ </a> </li>
-            <li> <b>Regionals Website:</b> <a href="https://maccdc.org/"> https://maccdc.org/ </a> </li>
-            <li> <b>Description:</b> 
-                The Collegiate Cyber Defense Competition (CCDC) is an annual event that challenges college and university students to protect a simulated corporate network from cyberattacks. The competition involves creating and defending a network against a red team of professional hackers while maintaining critical services for a simulated business. The teams are scored based on their ability to identify and mitigate security risks, respond to incidents, and maintain service availability.
             </li>
         </ul>
     </div>
