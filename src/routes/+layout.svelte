d319129be9f2d638704220425c3351c4e62e05ea
Neil Phan <nnp3axx@virginia.edu>
2023-04-16 00:17:20 -0400

    Fixed href issue in header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -25,40 +25,30 @@
 				<a
 					class="btn variant-ghost-surface"
 					href="/"
-					target="_blank"
-					rel="noreferrer"
 				>
 					Home
 				</a>
 				<a
 					class="btn variant-ghost-surface"
 					href="/events"
-					target="_blank"
-					rel="noreferrer"
 				>
 				    Events
 				</a>
 				<a
 					class="btn variant-ghost-surface"
 					href="/resources"
-					target="_blank"
-					rel="noreferrer"
 				>
 				    Resources
 				</a>
 				<a
 					class="btn variant-ghost-surface"
 					href="/officers"
-					target="_blank"
-					rel="noreferrer"
 				>
 				    Officers
 				</a>
 				<a
 					class="btn variant-ghost-surface"
 					href="https://discord.gg/87aKE8gqSc"
-					target="_blank"
-					rel="noreferrer"
 				>
 					Discord
 				</a>
