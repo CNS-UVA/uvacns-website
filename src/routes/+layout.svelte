f27f404986ec79aff217cd345ed91ab49d4dfd3b
Shreyas Mayya <smayya337@gmail.com>
2024-01-12 13:20:55 -0500

    adjust footer size, center text

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -55,8 +55,8 @@
     <!-- Page Route Content -->
     <svelte:fragment slot="pageFooter">
         <AppBar>
-                <p class="flex justify-center items-center text-xs/[4px]">Although this organization has members who are University of Virginia students and may have, University employees associated or engaged in its activities and affairs, the organization is not a part of or an agency. It is a separate and independent organization, which is responsible for and manages its own activities and affairs. The University does not direct, supervise or control the organization and is not responsible for the organization’s contracts, acts or omissions. </p>
-                <p class="flex justify-center items-center"> © CNS@UVA </p>
+                <p class="flex text-center items-center text-xs">Although this organization has members who are University of Virginia students and may have University employees associated or engaged in its activities and affairs, the organization is not a part or an agency of the University of Virginia. It is a separate and independent organization which is responsible for and manages its own activities and affairs. The University does not direct, supervise or control the organization and is not responsible for the organization’s contracts, acts or omissions. </p>
+                <p class="flex justify-center items-center text-sm pt-2"> © CNS@UVA </p>
         </AppBar>
     </svelte:fragment>
 	<slot />
