9fa42f01d740713fdb68a637550f97c83d4e73e6
Shreyas Mayya <shreyas@smayya.me>
2025-06-26 22:10:48 -0400

    remove stuff that got merged in unintentionally
    gotta love git...

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,10 +3,9 @@ module.exports = {
 	darkMode: 'class',
 	content: [
 		'./src/**/*.{html,js,svelte,ts}',
-		require('path').join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')
 	],
 	theme: {
 		extend: {}
 	},
-	plugins: [...require('@skeletonlabs/skeleton/tailwind/skeleton.cjs')()]
+	plugins: []
 };
