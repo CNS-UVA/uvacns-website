e252f650f7514c412b83b74545ac8e3a029b1b2d
Shreyas Mayya <shreyas@smayya.me>
2025-07-14 11:08:43 -0400

    set font to Inter/Fira Code

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,6 +10,10 @@
 <svelte:head>
 	<title>{config.title}</title>
 	<meta name="description" content="The cybersecurity club at the University of Virginia." />
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@300..700&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
 </svelte:head>
 
 <div class="flex min-h-screen flex-col">
