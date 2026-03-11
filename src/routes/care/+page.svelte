31faa447007b5dd416145880e04b2261c7b181f1
Shreyas Mayya <smayya337@gmail.com>
2026-03-11 19:23:02 -0400

    add actual video embed for CARE

--- src/routes/care/+page.svelte
+++ src/routes/care/+page.svelte
@@ -59,7 +59,6 @@
 	</div>
 </div>
 <div class="mt-8">
-     <h2 class="typography">Video</h2>
-     <!-- TODO: video -->
-      <iframe class="mt-8 mx-auto" width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ?si=tnyWtMdrUMC_0_e8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+	<h2 class="typography">Video</h2>
+	<iframe class="mt-8 mx-auto" width="560" height="315" src="https://www.youtube.com/embed/uzoguORdaig?si=jdGpVDna6zGppvaO" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 </div>
